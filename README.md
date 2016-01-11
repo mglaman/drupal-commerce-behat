c5e52feab9d3a30fa193e3df32a6e92cdd91caa5
Matt Glaman <nmd.matt@gmail.com>
2016-01-11 09:24:33 -0600

    Initial module
    Port the Behat items out of MojoCart.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Drupal Commerce Behat Extension [![Build Status](https://travis-ci.org/mglaman/drupal-commerce-behat.svg?branch=master)](https://travis-ci.org/mglaman/drupal-commerce-behat)
+
+Integrates with Behat Drupal Extension for Drupal Commerce support.
---
+++ tests/features/bootstrap/FeatureContext.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * The main Behat context.
+ */
+
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Behat\Behat\Context\SnippetAcceptingContext;
+
+/**
+ * Define application features from the specific context.
+ */
+class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
+
+}
